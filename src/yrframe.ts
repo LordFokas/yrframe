b2fbae06dad5c738707e1016aed20d9707d514b8
LordFokas <lordfokas@gmail.com>
2024-02-14 19:03:08 +0000

    expose Facade, update event-bus

--- src/yrframe.ts
+++ src/yrframe.ts
@@ -1,4 +1,5 @@
 export { Component } from './Component.js';
 export { ComponentEvents } from './ComponentEvents.js';
 export { ComponentFactory, FakeReact } from './ComponentFactory.js';
+export { Facade } from './Facade.js'
 export { Attributes, EventTarget } from './utils.js'
